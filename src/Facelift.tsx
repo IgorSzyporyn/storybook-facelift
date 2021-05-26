f8fcef2a2366cb61d5b7d8517055ecb6ea0d248d
IgorSzyporyn <igor.szyporyn@gmail.com>
2021-05-26 19:18:33 +0200

    fix: removed unused argument in callback

--- src/Facelift.tsx
+++ src/Facelift.tsx
@@ -64,7 +64,7 @@ export const Facelift = ({ api }: FaceliftProps) => {
         api.setOptions({ theme })
       }
     },
-    [api, setAddonState, addonState]
+    [api, setAddonState]
   )
 
   const toggleVariant = () => {
